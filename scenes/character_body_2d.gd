02c9fe9836fb3d359837ced44bfbaa72e922540d
IndexSH <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>
2025-05-29 13:14:02 -0300

    by lucas

---
+++ scenes/character_body_2d.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+
+const MOVESPEED = 500
+
+func _physics_process(delta: float) -> void:
+	var horizontal = Input.get_axis("ui_left","ui_right")
+	var vertical = Input.get_axis("ui_up","ui_down")
+	
+	position.x+=horizontal*MOVESPEED*delta
+	position.y+=vertical*MOVESPEED*delta
+	
+	move_and_slide()
