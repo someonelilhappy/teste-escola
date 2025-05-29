d7fdc97e46a3a4dde26aeb60609e04f72c4576eb
someonelilhappy <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>
2025-05-29 19:44:53 -0300

    basic script

---
+++ scripts/menu-script.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_play_button_pressed() -> void:
+	print("Botão 'julgar' precionado")
+
+
+func _on_continue_button_pressed() -> void:
+	print("Botão 'voltar pro inferno' precionado")
+
+
+func _on_exit_button_pressed() -> void:
+	print("Botão 'desistir' precionado")
